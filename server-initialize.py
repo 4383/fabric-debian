d48b618932ef2b1500fe1f31e8b8b1462322fa9e
Hervé BERAUD <herveberaud.pro@gmail.com>
2012-11-29 17:16:47 +0100

    postgresql evolution

--- server-initialize.py
+++ server-initialize.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+import md5
 
 env.hosts = ['192.168.56.101']
 
@@ -6,6 +7,9 @@ env.hosts = ['192.168.56.101']
 # Create new server operation
 ##########################################
 def initialize():
+    """
+    Full initialize of debia{
+    """
     install_lib()
 
 def install_lib():
@@ -27,11 +31,15 @@ def install_lib():
 # Common operations
 ##########################################
 def add_new_site():
-    username = prompt('Nom d\'utilisitateur ?')
+    username = prompt('Username :')
     run('adduser {0}' . format(username))
     add_postgre_user(username)
 
 def add_postgre_user(username):
+    passphrase = prompt('Digest pass phrase :')
+    password = md5.new()
+    password.update(passphrase)
+
     run('su - postgres')
-    run('''psql -c "CREATE USER {0} WITH PASSWORD '{1}';"''' . format(username, 'test'))
-    run('''pqsl -c "CREATE DATABASE {0}")
+    run('''psql -c "CREATE USER {0} WITH PASSWORD '{1}';"''' . format(username, password.hexdigest()))
+    run('''pqsl -c "CREATE DATABASE 'db'{0};"''' . format(username))
