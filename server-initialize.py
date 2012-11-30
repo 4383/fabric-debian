e3695a76e24df7d57b0b2cbc57fc9de335d8b971
Hervé BERAUD <herveberaud.pro@gmail.com>
2012-11-30 17:01:29 +0100

    [EVO] Add automaticaly configuration of new user

--- server-initialize.py
+++ server-initialize.py
@@ -8,11 +8,17 @@ env.hosts = ['192.168.56.101']
 ##########################################
 def initialize():
     """
-    Full initialize of debia{
+    Full initialize of debian server deployement
+    Only make basic action of the root user for make
+    functional server
     """
-    install_lib()
+    install_app()
 
-def install_lib():
+def install_app():
+    """
+    Install all apps to have functional server, that
+    works for python-django website with postgresql databse
+    """
     apps = [
         'python-pip',
         'python-virtualenv',
@@ -27,19 +33,45 @@ def install_lib():
     run('aptitude upgrade')
     run('aptitude install {0}' . format(apt))
 
+def set_firewall():
+    """
+    Initialize and configure iptable firewall.
+    """
+
 ##########################################
 # Common operations
 ##########################################
 def add_new_site():
+    """
+    Add a new website on the specified server.
+    The website live on new account user on this server.
+    The path to source is : /home/<username>/source
+    Create a new postgre user in relation with debian user.
+    """
     username = prompt('Username :')
     run('adduser {0}' . format(username))
     add_postgre_user(username)
+    add_httpd_vhost(username)
 
 def add_postgre_user(username):
+    """
+    Add new postgre user <username>
+    The password is a digest md5 of user passphrase
+    """
     passphrase = prompt('Digest pass phrase :')
     password = md5.new()
     password.update(passphrase)
 
-    run('su - postgres')
-    run('''psql -c "CREATE USER {0} WITH PASSWORD '{1}';"''' . format(username, password.hexdigest()))
-    run('''pqsl -c "CREATE DATABASE 'db'{0};"''' . format(username))
+    actions = [
+        '''psql -c "CREATE USER {0} WITH PASSWORD '{1}';"''' . format(username, password.hexdigest()),
+        '''psql -c "CREATE DATABASE db{0};"''' . format(username),
+        '''psql -c "GRANT ALL PRIVILEGES ON DATABASE db{0} to {0};"''' . format(username)
+    ]
+
+    for command in actions:
+        sudo(command, user='postgres')
+
+def add_httpd_vhost(username):
+    """
+    Add a new virtualhost to httpd
+    """
